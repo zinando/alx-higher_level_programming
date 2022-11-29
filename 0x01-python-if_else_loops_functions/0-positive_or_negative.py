517a28911adefc80a8831226a12efd30b8a73cd5
zinando <belovedsamex@yahoo.com>
2022-11-29 15:39:09 -0800

    Write a program will assign a random signed number to the variable number

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,10 +4,10 @@ import random
 number = random.randint(-10, 10)
 
 if number == 0:
-	val = "{} is zero".format(number)
+    val = "{} is zero".format(number)
 elif number > 0:
-	val = "{} is positive".format(number)
+    val = "{} is positive".format(number)
 else:
-	val = "{} is negative".format(number)
+    val = "{} is negative".format(number)
 
-print (val)
+print(val)
